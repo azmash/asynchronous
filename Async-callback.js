f0d2411a09579d4d0133485003f0c742925ccdef
Azma Sholiha <vy.phera@gmail.com>
2018-02-26 11:47:37 +0700

    Add files

---
+++ Async-callback.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+
+fs.readFile('simple.txt', 'utf8', function(error, contents){
+    console.log(contents);
+});
+console.log('End');
---
+++ Sync-callback.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+fs.readFile('simple.txt', 'utf8', function(error, contents){
+    console.log(contents); {
+        console.log('End')
+    }
+});
